20fc62c29a12c203d91b996ba45218b1c6ae24a1
za103 <za103@student.le.ac.uk>
2021-05-23 14:34:57 +0100

    fixed missing navigation elements

--- src/main/java/com/co3202/taskactivityplanner/controller/AccountController.java
+++ src/main/java/com/co3202/taskactivityplanner/controller/AccountController.java
@@ -1,12 +1,38 @@
 package com.co3202.taskactivityplanner.controller;
 
+import com.co3202.taskactivityplanner.dto.UserDto;
+import com.co3202.taskactivityplanner.model.User;
+import com.co3202.taskactivityplanner.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.context.request.WebRequest;
+
+import java.security.Principal;
+import java.util.UUID;
 
 @Controller
+@AllArgsConstructor
 public class AccountController {
-    @GetMapping("/account")
-    String showAccount() {
+
+    private UserService userService;
+
+    public String showAccount(Model model, Principal principal) {
+//         Authentication
+        String email = principal.getName();
+        User user = userService.getUserByEmail(email);
+//         Sending task object to thymeleaf, to access user details
+
+        model.addAttribute("user", user);
         return "account";
+    }
+
+    @GetMapping("/delete-user/{id}")
+    public String deleteUser(@PathVariable Principal principal) {
+        User user = userService.getUserByEmail(principal.getName());
+        userService.deleteUser(user.getId());
+        return "redirect:/logout";
     }
 }
